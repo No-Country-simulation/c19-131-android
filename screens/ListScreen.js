9dac12ddf87b5b4fb8448f8aedc7b5922c2543cb
poncegamez1 <poncegamez1@gmail.com>
2024-07-17 01:21:11 -0400

    Despliegue de lista de categorias y productos por categoría. Inputs en el login screen y llamado api.

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,15 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 const ListScreen = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { categoryId } = route.params;
+
+  const allProducts = [
+    { id: 1, name: "Product 1", category: 1 },
+    { id: 2, name: "Product 2", category: 1 },
+    { id: 3, name: "Product 3", category: 2 },
+    { id: 4, name: "Product 4", category: 2 },
+    { id: 5, name: "Product 5", category: 3 },
+    { id: 6, name: "Product 6", category: 4 },
+  ];
+
+  const products = allProducts.filter(product => product.category === categoryId);
+
+  const ProductItem = ({ name }) => {
+    return (
+      <Pressable style={{ marginVertical: 5 }} onPress={() => navigation.navigate('Detalle Producto')}>
+        <Text>{name}</Text>
+      </Pressable>
+    );
+  };
+
   return (
     <SafeAreaView>
-      <Text>ListScreen</Text>
+      <ScrollView style={{ marginHorizontal: 20, marginVertical: 10 }}>
+        {products.map(product => (
+          <ProductItem key={product.id} name={product.name} />
+        ))}
+      </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default ListScreen
+export default ListScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,104 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import { baseUrl } from '../utils/constants';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  function handleOnTapLoginButton() {
+    const userData = { email: email, password: password };
+    axios
+      .post(baseUrl + "/login", userData)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.status === 200) {
+          navigation.navigate("MainStack");
+          console.log("Ingreso exitoso");
+          setEmail("");
+          setPassword("");
+        }
+      })
+      .catch((error) => {
+        console.error("Login error:", error);
+        if (error.response && error.response.status === 401) {
+          alert("Credenciales inválidas.");
+        } else {
+          alert("Ocurrió un error. Intente de nuevo más tarde.");
+        }
+      });
+  }
   return (
     <SafeAreaView>
-      <Text>LoginScreen</Text>
+      <View style={{marginHorizontal: 20, marginTop: 30}}>
+        <View
+          style={{
+            borderWidth: 1,
+            borderRadius: 5,
+            borderColor: "black",
+            fontSize: 14,
+            height: 41,
+            padding: 10,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            style={{
+              flex: 1,
+              fontSize: 14,
+            }}
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Inserte email"
+            placeholderTextColor="#C0C0C0"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+        <View
+          style={{
+            borderWidth: 1,
+            borderRadius: 5,
+            borderColor: "black",
+            fontSize: 14,
+            height: 41,
+            padding: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 10,
+          }}
+        >
+          <TextInput
+            style={{
+              flex: 1,
+              fontSize: 14,
+            }}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Inserte contraseña"
+            placeholderTextColor="#C0C0C0"
+            autoCapitalize="none"
+            secureTextEntry={true}
+          />
+        </View>
+      </View>
 
-      <Pressable onPress={() => navigation.navigate('Register')}>
+      <Pressable onPress={handleOnTapLoginButton}>
+        <Text>Log in</Text>
+      </Pressable>
+
+      <Pressable onPress={() => navigation.navigate("Register")}>
         <Text>Register Screen</Text>
       </Pressable>
 
-      <Pressable onPress={() => navigation.navigate('MainStack')}>
-        <Text>Log in</Text>
-      </Pressable>
+      
     </SafeAreaView>
-  )
+  );
 }
 
 export default LoginScreen
