c70600b00f3881f6944a8d351eee2fc791fe5291
poncegamez1 <poncegamez1@gmail.com>
2024-07-25 14:04:58 -0400

    Creacion de la pantalla de registro.

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,38 +1,98 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useNavigation } from '@react-navigation/native';
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import { Button, TextInput } from "react-native-paper";
 
 const RegisterScreen = () => {
   const navigation = useNavigation();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPasswords] = useState("");
   return (
     <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
-      <View style={{ alignItems: "center", marginTop: 80 }}>
-        <Text style={{ fontSize: 40, fontWeight: "bold" }}>Bienvenido</Text>
-      </View>
+      <KeyboardAvoidingView style={{ marginHorizontal: 30 }}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View
+            style={{ alignItems: "center", marginTop: 80, marginBottom: 30 }}
+          >
+            <Text style={{ fontSize: 40, fontWeight: "bold" }}>Bienvenido</Text>
+          </View>
 
-      <View>
-        
-      </View>
+          <View>
+            <TextInput
+              label="Nombre"
+              mode="outlined"
+              value={firstName}
+              onChangeText={setFirstName}
+              keyboardType="default"
+            />
+            <TextInput
+              label="Apellido"
+              mode="outlined"
+              value={lastName}
+              onChangeText={setLastName}
+              keyboardType="default"
+              style={{ marginTop: 30 }}
+            />
+            <TextInput
+              label="Dirección"
+              mode="outlined"
+              value={address}
+              onChangeText={setAddress}
+              keyboardType="default"
+              style={{ marginTop: 30 }}
+            />
+            <TextInput
+              label="Teléfono"
+              mode="outlined"
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+              keyboardType="numeric"
+              style={{ marginTop: 30 }}
+            />
+            <TextInput
+              label="Correo Electrónico"
+              mode="outlined"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="default"
+              style={{ marginTop: 30 }}
+            />
+            <TextInput
+              label="Contraseña"
+              mode="outlined"
+              value={password}
+              onChangeText={setPasswords}
+              keyboardType="default"
+              autoCapitalize="none"
+              style={{ marginTop: 30 }}
+              secureTextEntry={true}
+            />
+          </View>
 
-      <Pressable
-        style={{
-          backgroundColor: "#594c80",
-          borderRadius: 20,
-          alignItems: "center",
-          justifyContent: "center",
-          height: 40,
-          marginVertical: 20,
-          marginHorizontal: 65,
-        }}
-        onPress={() => navigation.navigate("MainStack")}
-      >
-        <Text style={{ color: "white", fontSize: 14 }}>Registrarse</Text>
-      </Pressable>
+          <Button mode="contained" style={{ marginTop: 50, marginBottom: 30 }} onPress={() => navigation.navigate('MainStack')}>
+            Registrarse
+          </Button>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
-}
+};
 
-export default RegisterScreen
+export default RegisterScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
